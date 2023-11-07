d6853e0c8a13e87ab618bd9202048298fb23ec86
Haduin <36509113+Haduin@users.noreply.github.com>
2023-11-07 20:34:58 +0100

    added tailwind and redux-toolkit libs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="flex bg-cyan-500 sm:bg-red-500 md:bg-red-900 justify-between ">
+            <button> Akcja 1</button>
+            <button> Akcja 2</button>
+        </div>
     </div>
   );
 }
