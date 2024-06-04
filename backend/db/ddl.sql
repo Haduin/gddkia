0268fbad9b7e887d72ff9cd94d3a06189c3bc7f0
Haduin <jakub.chodan2@gmail.com>
2024-06-04 17:10:42 +0200

    update docker ddl.sql

--- backend/db/ddl.sql
+++ backend/db/ddl.sql
@@ -1,22 +1,41 @@
-create table branch
+create table if not exists branch
 (
-    branch_id   bigserial primary key,
-    region_name varchar(255)
+    geo_coordinatex                       double precision not null,
+    geo_coordinatey                       double precision not null,
+    managed_network_length                double precision not null,
+    branch_id                             bigserial
+        primary key,
+    address_street                        varchar(255),
+    address_zip_city                      varchar(255),
+    branch                                varchar(255),
+    city                                  varchar(255),
+    notes                                 varchar(255),
+    phone                                 varchar(255),
+    region                                varchar(255),
+    road_number                           varchar(255),
+    road_section_at_district_headquarters varchar(255),
+    section                               varchar(255)
 );
 
-
-create table estimate
+create table if not exists estimate
 (
     date_from     timestamp(6) with time zone,
     date_to       timestamp(6) with time zone,
     estimate_id   bigserial
         primary key,
-    region_id     bigint
-        references branch,
+    road_length   bigint,
     contract_name varchar(255)
 );
 
-create table group_table
+create table if not exists branch_estimate
+(
+    branch_id   bigint not null
+        references branch,
+    estimate_id bigint not null
+        references estimate
+);
+
+create table if not exists group_table
 (
     estimate_id bigint
         references estimate,
@@ -25,12 +44,11 @@ create table group_table
     group_name  varchar(255)
 );
 
-create table jobs
+create table if not exists jobs
 (
     cost_estimate double precision default 1,
     quantity      double precision,
-    group_id      bigint
-        references group_table,
+    group_id      bigint            references group_table,
     job_id        bigserial
         primary key,
     description   varchar(255),
@@ -39,7 +57,7 @@ create table jobs
     unit          varchar(255)
 );
 
-create table users
+create table if not exists users
 (
     id       bigserial
         primary key,
